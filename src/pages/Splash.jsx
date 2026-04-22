850328e23ffef68a7e20933c88a5bf40a3094ef7
Christopher Doyle <scorptonic@gmail.com>
2026-04-22 16:10:34 -0500

    embed Google Form below match ticker

--- src/pages/Splash.jsx
+++ src/pages/Splash.jsx
@@ -18,6 +18,22 @@ export default function Splash() {
           <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.8 }}>
             <HeroSection />
             <MatchTicker />
+            <section className="bg-background px-6 py-12">
+              <div className="max-w-2xl mx-auto">
+                <iframe
+                  src="https://docs.google.com/forms/d/e/1FAIpQLSf6u2F4MqT1rAE5yHWStIQ-H9AEa2ceOvpnFvCT5-Vs7ebTDw/viewform?embedded=true"
+                  width="100%"
+                  height="600"
+                  frameBorder="0"
+                  marginHeight="0"
+                  marginWidth="0"
+                  title="FIFA KC Watch Party Sign Up"
+                  className="rounded-xl"
+                >
+                  Loading…
+                </iframe>
+              </div>
+            </section>
             <SEOFooter />
           </motion.div>
         )}
